f0f28b9aa970ee52aeda3b08dfed60417cec058a
osayamen <mailbuddie@gmail.com>
2019-04-06 19:25:04 -0400

    adding new files

--- app.py
+++ app.py
@@ -4,13 +4,14 @@ import pandas as pd
 import preprocessor as p
 
 
-def aggregage_user_data(user: str)-> pd.DataFrame:
+def aggregage_user_data(user: str) -> pd.DataFrame:
     target_user = Handle(user)
     dc = DataCollectionService()
 
-    targets_followers = dc.get_users_followers(screen_name = target_user.username, count = 5)
-    df = pd.DataFrame(columns=["followers_screen_name","tweet_text", "harsh_tags", "user_description", "followers_description",
-                               "friends_description", "mentions_descriptions", "retweeted_descriptions"])
+    targets_followers = dc.get_users_followers(screen_name=target_user.username, count=5)
+    df = pd.DataFrame(
+        columns=["followers_screen_name", "tweet_text", "harsh_tags", "user_description", "followers_description",
+                 "friends_description", "mentions_descriptions", "retweeted_descriptions"])
     for follower in targets_followers:
         tweets_data = dc.get_aggregate_timeline_text(follower, 200)
         friends_ids = dc.get_users_friends(user_id=follower)
@@ -18,19 +19,22 @@ def aggregage_user_data(user: str)-> pd.DataFrame:
         user_description = dc.get_users_descriptions(user_ids=[follower])
         friends_description = dc.get_users_descriptions(user_ids=friends_ids) if friends_ids else ""
         followers_description = dc.get_users_descriptions(user_ids=followers_ids) if followers_ids else ""
-        mentions_descriptions = dc.get_users_descriptions(screen_names=tweets_data.mentions) if tweets_data.mentions else ""
-        retweeted_descriptions = dc.get_users_descriptions(screen_names=tweets_data.handles_retweeted) if tweets_data.handles_retweeted else ""
+        mentions_descriptions = dc.get_users_descriptions(
+            screen_names=tweets_data.mentions) if tweets_data.mentions else ""
+        retweeted_descriptions = dc.get_users_descriptions(
+            screen_names=tweets_data.handles_retweeted) if tweets_data.handles_retweeted else ""
         df = df.append({
-                    "followers_screen_name": tweets_data.screen_name,
-                    "tweet_text": tweets_data.tweet_text,
-                    "harsh_tags": " ".join(tweets_data.hashtags),
-                    "user_description": " ".join(user_description),
-                    "followers_description": " ".join(followers_description),
-                    "friends_description": " ".join(friends_description),
-                    "mentions_descriptions": " ".join(mentions_descriptions),
-                    "retweeted_descriptions": " ".join(retweeted_descriptions)}, ignore_index=True)
+            "followers_screen_name": tweets_data.screen_name,
+            "tweet_text": tweets_data.tweet_text,
+            "harsh_tags": " ".join(tweets_data.hashtags),
+            "user_description": " ".join(user_description),
+            "followers_description": " ".join(followers_description),
+            "friends_description": " ".join(friends_description),
+            "mentions_descriptions": " ".join(mentions_descriptions),
+            "retweeted_descriptions": " ".join(retweeted_descriptions)}, ignore_index=True)
         df.to_csv("tweets_info.csv")
-    return  df
+    return df
+
 
 def preprocess(df):
     df.apply(p.clean)
@@ -41,4 +45,3 @@ user = "etsu"
 aggregage_user_data(user)
 # df = pd.read_csv("tweets_info.csv")
 # preprocess(df)
-
